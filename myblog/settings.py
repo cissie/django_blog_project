cabd891e6c42be45974f54aea5771eadd07c5659
cissie <cissie@gmail.com>
2015-01-10 18:36:02 -0800

    added css

--- myblog/settings.py
+++ myblog/settings.py
@@ -95,7 +95,13 @@ STATIC_ROOT = 'staticfiles'
 
 DEBUG = False
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 try:
     from .local_settings import *
 except ImportError:
     pass
+
+
