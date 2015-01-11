430129ab104095b24c4cc100a197ec373c34281a
cissie <cissie@gmail.com>
2015-01-10 20:14:36 -0800

    added a form for editing and adding posts within the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^new/post/$', views.new_post, name='new_post'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 )
